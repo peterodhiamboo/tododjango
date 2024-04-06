d5182f74f71a6784087120ddd45121a63ca46027
peterodhiamboo <peterodhiamboo@gmail.com>
2024-04-06 08:58:14 -0400

    working on template data parsing from backend

--- tododjango/home/views.py
+++ tododjango/home/views.py
@@ -2,5 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+task_posts = [
+    {
+        'task_category' :  'SOCIAL',
+        'task_date': '6th March 2024',
+        'task_context' : 'This is just for educational purposes'
+    },
+    {
+        'task_category' :  'COORPORATE',
+        'task_date': '7th March 2024',
+        'task_context' : 'This is just for  purposes'
+    }
+]
+
 def home(request):
-    return render(request, 'index.html', {})
+    context = {
+        'task' : task_posts
+    }
+    
+    return render(request, 'index.html', context)
